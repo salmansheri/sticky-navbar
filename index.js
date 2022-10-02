3394363f3cd0761d8bece1e04de084ac5a57a692
salmansheri <sheriffsalman00@gmail.com>
2022-10-02 15:08:36 +0530

    second commit

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
 // return the nav bar in javascript using queryselector method
 const navBarEl = document.querySelector(".navbar");
 
-// to get the position of the bottom container 
-const bottomContainerEl = document.querySelector(".bottom-container"); 
+// to get the position of the bottom container use <bottomContainerEl.offsetTop>
+const bottomContainerEl = document.querySelector(".bottom-container");
 console.log(bottomContainerEl.offsetTop);
 
-// to get the height of the height of the navigation bar
-// console.log(navBarEl.offsetHeight); 
-
-
+// to get the height of the height of the navigation bar use <navBarEl.offsetHeight>
+// console.log(navBarEl.offsetHeight);
 
 // print the navbar element
-
 
 // adding event listener for the scroll event
 window.addEventListener("scroll", function () {
   // targetting the scroll dimenension "y" by window.scrollY
-//   check the condition if this.window.scrollY is greater than the bottomContainerEl.offsetTop -
-if (window.scrollY > bottomContainerEl.offsetTop - navBarEl.offsetHeight - 50) {
-    // setting navbar "classList to active" can make the navbar color change  
-    navBarEl.classList.add("active"); 
-
-} else {
-    navBarEl.classList.remove("active"); 
-}
-
-
-
-
-}); 
+  //   check the condition if this.window.scrollY is greater than the bottomContainerEl.offsetTop -
+  if (
+    window.scrollY >
+    bottomContainerEl.offsetTop - navBarEl.offsetHeight - 50
+  ) {
+    // setting navbar "classList to active" can make the navbar color change
+    navBarEl.classList.add("active");
+  } else {
+    navBarEl.classList.remove("active");
+  }
+});
